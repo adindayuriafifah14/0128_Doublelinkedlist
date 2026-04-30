da08f8c41c3811faedfbcb83c21103cf8680f5ad
adindayuriafifah14 <adindaafifah14@gmail.com>
2026-04-30 12:53:42 +0700

    Menambahkan fungsi retransverse untuk traversal backward dari akhir ke awal

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -152,5 +152,34 @@ public:
         }
     }
 
+    void retransverse()
+    {
+        if (START == NULL)
+        {
+            cout << "\nList is empty" << endl;
+            return;
+        }
+
+        //step 1:Move to last node
+        Node *currentNode = START;
+        int i = 0;
+        while (currentNode->next != NULL)
+        {
+            currentNode = currentNode->next;
+            i++;
+        }
+
+        //step 2: Transverse backward
+        cout << "\nRecords in descending order of roll number are:\n";
+        while (currentNode != NULL)
+        {
+            cout << i + 1 << ". " << currentNode->noMhs << " " <<endl;
+
+            //step 3: Move to previous node
+            currentNode = currentNode->prev;
+            i--;
+        }
+    }
+
 
 }
