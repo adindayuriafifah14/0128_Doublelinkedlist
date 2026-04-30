73201c989f503f8b8b68abf3667f1789f91467e5
adindayuriafifah14 <adindaafifah14@gmail.com>
2026-04-30 12:00:01 +0700

    Menambahkan pengecekan duplikat dan logika insert di awal untuk DoubleLinkedList

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -20,4 +20,40 @@ public:
     {
         START = NULL;
     }
+
+    void addNode()
+    {
+        int nim;
+        cout << "\nEnter the roll number of the student: ";
+        cin >> nim;
+
+        //step 1: Allocate memory for new node
+        Node *newNode = new Node();
+
+        //step 2: Assign value to the data new node
+        newNode->noMhs = nim;
+
+        //step 3: Insert at beginning if list is empty or nim is smallest
+        if (START == NULL || nim <= START ->noMhs)
+        {
+            if (START != NULL && nim == START->noMhs)
+            {
+                cout << "\nDuplicate number not allowed" << endl;
+                return;
+            }
+            //step 4: newNode.next = START
+            newNode->next = START;
+
+            //step 5: START.prev = newNode (if START exists)
+            if (START != NULL)
+                START->prev = newNode;
+
+            //step 6: newNode.prev = NULL
+            newNode->prev = NULL;
+
+            //step 7: START = newNode
+            START = newNode;
+            return;
+        }
+    }
 }
