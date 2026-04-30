4cf749b4a919dafccd7e155cfd330abc92567701
adindayuriafifah14 <adindaafifah14@gmail.com>
2026-04-30 12:34:00 +0700

    Menambahkan traversal, logika penghapusan lengkap (awal/tengah/akhir), dan memory deallocation pada fungsi hapus

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -86,7 +86,7 @@ public:
         if (START == NULL)
         {
             cout << "\nList is empty" << endl;
-            return:
+            return;
         }
 
         cout << "\nEnter the roll number of the student whose record is to be deleted: ";
@@ -94,5 +94,38 @@ public:
         cin >> rollNo;
 
         Node *current = START;
+
+        //step 1: Transverse the list to find the node
+        while (current != NULL && current->noMhs != rollNo)
+            current = current->next;
+
+        if (current == NULL)
+        {
+            cout << "Record not found" << endl;
+            return;
+        }
+
+        //step 2: If node is at the beginning
+        if (current == START)
+        {
+            START = current->next;//step 2a: START = START.next
+            if (START != NULL)
+                START->prev = NULL;//step 2b: START.prev = NULL
+        }
+        else
+        {
+            //step 3: Link previous node to next of current
+            current->prev->next = current->next;
+
+            //step 4: If current is not the last node
+            if (current->next != NULL)
+                current->next->prev = current->prev;
+        }
+
+        //step 5: Delete the Node
+        delete current;
+        cout << "Record with roll number " << rollNo << " deleted" << endl;
     }
+
+
 }
